eb6fe52113f217e220919eb9c1bca16003feef90
hamidkhattakkhan <168951768+hamidkhattakkhan@users.noreply.github.com>
2024-05-17 22:27:50 -0700

    hAMID

--- javasc/script.js
+++ javasc/script.js
@@ -1,12 +1,15 @@
-const container = document.getElementById('container');
-const registerBtn=document. getElementById('register');
-const loginBTN = document.getElementById('login');
-registerBtn.addEventListener('click' , ()=>
-{
-    container.classList.add("active");
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const signUpButton = document.getElementById("register");
+    const signInButton = document.getElementById("login");
+    const container = document.getElementById("container");
 
-lo.addEventListener('click' , ()=>
-{
-    container.classList.remove("active");
-})
+    signUpButton.addEventListener("click", function() {
+        container.classList.add("sign-up");
+        container.classList.remove("sign-in");
+    });
+
+    signInButton.addEventListener("click", function() {
+        container.classList.remove("sign-up");
+        container.classList.add("sign-in");
+    });
+});
